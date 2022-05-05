3d1bd11dd898c061df10bfaa7990dc2794417336
agengayuu <ayuageng@apps.ipb.ac.id>
2022-05-05 19:59:52 +0700

    Authentication, untuk membatasi hak akses user

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/warga',[WargaController::class,'index']);
-Route::get('/warga/create',[WargaController::class,'create']);
-Route::post('/warga/store',[WargaController::class,'store']);
-Route::get('/warga/{id}/edit',[WargaController::class,'edit']);
-Route::put('/warga/{id}',[WargaController::class,'update']);
-Route::delete('/warga/{id}',[WargaController::class,'destroy']);
+Route::middleware(['auth'])->group(function(){
+    Route::get('/warga',[WargaController::class,'index']);
+    Route::get('/warga/create',[WargaController::class,'create']);
+    Route::post('/warga/store',[WargaController::class,'store']);
+    Route::get('/warga/{id}/edit',[WargaController::class,'edit']);
+    Route::put('/warga/{id}',[WargaController::class,'update']);
+    Route::delete('/warga/{id}',[WargaController::class,'destroy']);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
